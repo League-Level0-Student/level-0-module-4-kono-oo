7116a3c2c814ab7f119e5732c90afa35eef28f4c
aktrooper9 <andrewkim.bc@gmail.com>
2023-09-30 14:54:07 -0700

    done

--- src/_03_char/_3_pi_aloud/PiAloud.java
+++ src/_03_char/_3_pi_aloud/PiAloud.java
@@ -8,11 +8,16 @@ public class PiAloud {
      * If you cannot play sound on this computer, set canPlaySounds to false.
      * If you are not sure, ask your teacher
      * *****************/
-    boolean canPlaySounds = true;
+    boolean canPlaySounds = false;
 
 
     // 1. Make a main method and make sure your program can run
-
+    public static void main(String[] args) {
+    	String pi = "3.141592653589793238";
+    	for(int i =0; i<20; i++) {
+    		System.out.println(pi.charAt(i));
+    	}
+    }
     // 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
 
     // 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
